ee45b5daa1bd658d3d173f4446fd1cffa70caac1
Tyler Pollard <tylerpollard410@gmail.com>
2024-11-13 22:42:54 -0500

    Add dashboard layout

--- R/app_ui.R
+++ R/app_ui.R
@@ -50,8 +50,103 @@ app_ui <- function(request) {
     # Leave this function for adding external resources
     golem_add_external_resources(),
     # Your application UI logic
-    dashboardPage(
-      h4("NFL APP")
+    dashboardPage(dark = NULL,
+                  footer = dashboardFooter(left = br()),
+                  freshTheme = my_theme,
+                  preloader = list(html = tagList(spin_1(), "Loading ..."), color = "#3c8dbc"),
+
+                  # Dahsboard Header ===============
+                  header = dashboardHeader(
+                    title = dashboardBrand(
+                      title = div(style = "font-size:14pt",
+                                  align = "center",
+                                  "NFL Analysis App",
+                                  dashboardBadge(
+                                    color = "warning",
+                                    rounded = TRUE,
+                                    position = "right",
+                                    "v1.0")
+                      ),
+                      color = "primary"
+                    ),
+                    compact = FALSE,
+                    rightUi = tags$li(
+                      class = "dropdown",
+                      dropdownMenu(
+                        badgeStatus = NULL,
+                        type = "notifications",
+                        headerText = "NFL Analysis",
+                        icon = icon("info-circle"),
+                        notificationItem(
+                          inputId = "info1",
+                          text = "Developer: Tyler Pollard",
+                          icon = icon("users-cog"),
+                          status = "info"
+                        ),
+                        notificationItem(
+                          inputId = "info2",
+                          text = "Release Date: 20 Oct 2024",
+                          icon = icon("calendar"),
+                          status = "info"
+                        ),
+                        notificationItem(
+                          inputId = "info3",
+                          text = "Version: 1.0",
+                          icon = icon("code"),
+                          status = "info"
+                        )
+                      )
+                    ),
+                    ## Navbar Menu ------------------
+                    navbarMenu(
+                      id = "navMenu",
+                      ### Home Tab ----
+                      navbarTab(tabName = "homeTab", text = "Home")
+                    ) # end navbarMenu
+                  ), # close header
+                  scrollToTop = TRUE,
+                  # Dashboard Sidebar =============
+                  sidebar = dashboardSidebar(
+                    id = "sidebar",
+                    skin = "dark",
+                    elevation = 5,
+                    fixed = FALSE,
+                    minified = FALSE,
+                    status = "primary",
+                    compact = TRUE,
+                    collapsed = TRUE,
+                    width = "150px",
+                    ## Sidebar Menu ---------------
+                    sidebarMenu(
+                      id = "menu_items",
+                      ### Data Tab ----
+                      h4("Data", style = "color: white"),
+                      menuItem(text = "Standings", tabName = "standingsTab", icon = icon("table")),
+                      menuItem(text = "Scores", tabName = "scoresTab", icon = icon("table")),
+                      menuItem(text = "Team Statistics", icon = icon("users"),
+                               menuSubItem(text = "Offense", tabName = "teamOffenseTab"),
+                               menuSubItem(text = "Defense", tabName = "teamDefenseTab"),
+                               menuSubItem(text = "Special Teams", tabName = "teamSpecialTeamsTab"),
+                               menuSubItem(text = "Scoring", tabName = "teamScoringTab")
+                      ),
+                      menuItem(text = "Player Statistics", icon = icon("user"),
+                               menuSubItem(text = "Offense", tabName = "playerOffenseTab"),
+                               menuSubItem(text = "Defense", tabName = "playerDefenseTab"),
+                               menuSubItem(text = "Special Teams", tabName = "playerSpecialTeamsTab"),
+                               menuSubItem(text = "Scoring", tabName = "playerScoringTab"),
+                               menuSubItem(text = "Fantasy", tabName = "playerFantasyTab")
+                      ),
+                      h4("Betting", style = "color: white"),
+                      menuItem(text = "Games", tabName = "bettingGamesTab", icon = icon("user")),
+                      menuItem(text = "Player Props", tabName = "bettingPlayerPropsTab", icon = icon("user"))
+                    ) # close sidebar menu
+                  ), # close dashboard sidebar
+                  # Dashboard Controlbar ==================
+                  controlbar = dashboardControlbar(),
+                  # Dashboard Body ================
+                  body = dashboardBody(
+                    useShinyjs()
+                  )
     )
   )
 }
